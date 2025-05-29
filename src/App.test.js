b34a20658fb2cf3448bf4ef3018e82e929311189
unknown <wilmerrestrepoo@hotmail.com>
2025-05-28 20:16:00 -0500

    Quinto commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Componente App', () => {
+  test('muestra una alerta al hacer clic en el botón', () => {
+    // Simula la función alert
+    window.alert = jest.fn();
+
+    // Renderiza el componente
+    render(<App />);
+
+    // Busca el botón por su texto
+    const boton = screen.getByText('¡Personal Web site!');
+
+    // Simula un clic en el botón
+    fireEvent.click(boton);
+
+    // Verifica que se haya llamado a alert con el mensaje correcto
+    expect(window.alert).toHaveBeenCalledWith('¡Personal Web site!');
+  });
 });
