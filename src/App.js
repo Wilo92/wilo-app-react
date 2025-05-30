e79fccfe96d1d79dbf7a018e953a852439cd1882
unknown <wilmerrestrepoo@hotmail.com>
2025-05-29 22:41:27 -0500

    31546564546

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const images = [
   "https://www.ucp.edu.co/wp-content/uploads/2025/01/Logo_50_ucp-1.webp"
 ];
 
-/*function App() {
+function App() {
   const [currentImage, setCurrentImage] = useState(0);
 
   useEffect(() => {
@@ -17,7 +17,7 @@ const images = [
   }, []);
 
   const handleClick = () => {
-    alert('¡Personal Web site!');
+    alert('¡Personal W site!');
   };
 
   return (
