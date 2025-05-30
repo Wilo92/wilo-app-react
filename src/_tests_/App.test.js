8838e041945a41d2e6cb5a6bc794739b866889e3
unknown <wilmerrestrepoo@hotmail.com>
2025-05-29 20:58:59 -0500

    454512

--- src/_tests_/App.test.js
+++ src/_tests_/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import App from './App';
+import App from '../App';
 
 describe('Componente App', () => {
   test('muestra una alerta al hacer clic en el botón', () => {
